0c3fa12d0f0ba395a67dc0f4e868aa0c2625ac5e
kamalbsr2101@gmail.com <kamalbsr2101@gmail.com>
2023-10-13 09:53:15 +0530

    Add client side code by first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,54 @@
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import './App.css';
+import fetchDataFromApi from "./utils/api.js";
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice.js';
+// import all files
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Home from "./pages/home/Home.jsx";
+import Details from "./pages/details/Details.jsx";
+import Explore from "./pages/explore/Explore.jsx";
+import Error from "./pages/error404/Error.jsx";
+import SearchResult from "./pages/searchResults/SearchResults.jsx";
+
+
+function App() {
+  const dispatch = useDispatch()
+  const url = useSelector((state) => state.home.url)
+  // console.log(url)
+
+  useEffect(() => {
+    fetchApiConfig();
+  }, []);
+
+  const fetchApiConfig = () => {
+    fetchDataFromApi('/configuration').then((res) => {
+      console.log(res)
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      }
+
+      dispatch(getApiConfiguration(url))
+    })
+  };
+
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:mediaType/:id' element={<Details />} />
+        <Route path='/search/:query' element={<SearchResult />} />
+        <Route path='/explore/:mediaType' element={<Explore />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/src/pages/error404/Error.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./style.scss";
+
+const Error = () => {
+    return (
+        <div>Error</div>
+    )
+}
+
+export default Error
